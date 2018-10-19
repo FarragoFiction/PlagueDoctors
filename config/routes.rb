1f4e108b4784a5af1a64cfb83b3987d515646ced
jadedResearcher <gtg287y@gmail.com>
2018-10-18 22:07:47 -0400

    new root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :paldemic_files
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'paldemic_files#index'
 end
