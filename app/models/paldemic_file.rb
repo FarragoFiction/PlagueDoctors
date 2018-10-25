42a820fe344db21286a10160f2460363f47861e5
jadedResearcher <gtg287y@gmail.com>
2018-10-25 19:35:32 -0400

    working on restyling

--- app/models/paldemic_file.rb
+++ app/models/paldemic_file.rb
@@ -1,6 +1,10 @@
 class PaldemicFile < ApplicationRecord
   @@admin_pw = "jrbutitsforareallygoodcause"
 
+  def total_votes
+    return num_upvotes - num_downvotes;
+  end
+
   #takes in a string, figures out of it at least starts the way a paldemic file should
   # not gonna bother fully parsing it yet
   # i wonder how you'd even do lz compression in ruby? would i need to drag that javascript library even further?
