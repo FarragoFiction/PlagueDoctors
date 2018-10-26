e1be963b4cbed7b317ca80681adc19c9e5c8564b
jadedResearcher <gtg287y@gmail.com>
2018-10-26 15:09:04 -0400

    while sqlite doesn't mind this faulty migration existing, postgrl or whatever heroku uses does so deleted

--- db/migrate/20181026181443_add_num_views_to_paldemic_files.rb
+++
@@ -1,6 +0,0 @@
-class AddNumViewsToPaldemicFiles < ActiveRecord::Migration[5.0]
-  def change
-    #why did it even let me accidentally do it backwards? bluuuuuuuh
-    add_column :paldemic_files,:num_views,:integer, :null => false, :default => 0
-  end
-end
