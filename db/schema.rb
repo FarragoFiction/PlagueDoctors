b951814a33cb423634bfb88309b0db1864e6e009
jadedResearcher <gtg287y@gmail.com>
2018-11-06 08:45:09 -0500

    time to test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181026193750) do
+ActiveRecord::Schema.define(version: 20181106134149) do
 
   create_table "paldemic_files", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,18 @@ ActiveRecord::Schema.define(version: 20181026193750) do
     t.         "integer",       default: "0",                                         null: false
     t.integer  "num_views",     default: 0,                                           null: false
     t.string   "description",   default: "A really cool paldemic file, I promise!!!", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
