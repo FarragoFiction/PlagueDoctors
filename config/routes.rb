df8d5165a1ef31eb0f50ccfb11c53a631c1f30e8
jadedResearcher <gtg287y@gmail.com>
2018-11-06 13:21:39 -0500

    more steps

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,8 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'paldemic_files#index'
+
+  # Routes for Google authentication
+  get "auth/:provider/callback", to: "sessions#googleAuth"
+  get "auth/failure", to: redirect("/")
 end
