d7dfe2114dbe2e580b27b6a7f63ac1fe1eb52afb
jadedResearcher <gtg287y@gmail.com>
2018-11-21 19:56:20 -0500

    data base seeds set up

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+karkat = '{\"lastPlayed\":\"1542840462569\",\"isempress\":\"false\",\"hatchDate\":\"1542840462569\",\"lastFed\":\"1542840462569\",\"dollDATAURL\":\"Karkat Vantas:___HBRpaWn_AADuAABpaWmZmZmJiYn_uin_uikAAAAAAABLS0s6OjoREREAAAAREREzMzPExMT_AAB_AAAIgrBWN_4eDwWC2A==\",\"boredomJson\":\"0\",\"nameJSON\":\"Karkat Vantas\",\"healthJson\":\"100\",\"TYPE\":\"GRUB\",\"corrupt\":\"false\",\"purified\":\"false\",\"patience\":\"0\",\"idealistic\":\"0\",\"curious\":\"0\",\"loyal\":\"113\",\"energetic\":\"0\",\"external\":\"0\",\"remembered\":\"{}\",\"rememberedNames\":\"{}\",\"rememberedCastes\":\"{}\"}","breeder":"ParadoxSpace'
+feferi = '{\"lastPlayed\":\"1542843765974\",\"isempress\":\"false\",\"hatchDate\":\"1542843765974\",\"lastFed\":\"1542843765974\",\"dollDATAURL\":\" Feferi Peixes:___HBSZAE2xEmL_qeUA-lEEsj7_qeX_uin_uimZAE0AAAAA3v43qskREREuLi6xEmKZAE3ExMSZAE13ACsIgsBYOgqEBQKcJhQKAkBJgA==\",\"boredomJson\":\"0\",\"nameJSON\":\" Feferi Peixes\",\"healthJson\":\"100\",\"TYPE\":\"GRUB\",\"corrupt\":\"false\",\"purified\":\"false\",\"patience\":\"0\",\"idealistic\":\"0\",\"curious\":\"0\",\"loyal\":\"0\",\"energetic\":\"113\",\"external\":\"0\",\"remembered\":\"{}\",\"rememberedNames\":\"{}\",\"rememberedCastes\":\"{}\"}","breeder":"ParadoxSpace"}'
+terezi = '{\"lastPlayed\":\"1542843765974\",\"isempress\":\"false\",\"hatchDate\":\"1542843765974\",\"lastFed\":\"1542843765974\",\"dollDATAURL\":\"Terezi Pyrope:___YU8QAAAIKCAGBgAIKCAGBgAEBA8QAA8QAAAIKCAAAAAAAASkpKERERSkpK8QAApAYIxMTEAIKCAGBgCILwXgmFg-_ASQsWAogFFg",\"boredomJson\":\"0\",\"nameJSON\":\" Terezi Pyrope\",\"healthJson\":\"100\",\"TYPE\":\"GRUB\",\"corrupt\":\"false\",\"purified\":\"false\",\"patience\":\"0\",\"idealistic\":\"0\",\"curious\":\"0\",\"loyal\":\"0\",\"energetic\":\"0\",\"external\":\"113\",\"remembered\":\"{}\",\"rememberedNames\":\"{}\",\"rememberedCastes\":\"{}\"}","breeder":"ParadoxSpace"}'
 
-TimeHole.create(permanent: true, wigglerJSON: '{\"lastPlayed\":\"1542840462569\",\"isempress\":\"false\",\"hatchDate\":\"1542840462569\",\"lastFed\":\"1542840462569\",\"dollDATAURL\":\"Karkat Vantas:___HBRpaWn_AADuAABpaWmZmZmJiYn_uin_uikAAAAAAABLS0s6OjoREREAAAAREREzMzPExMT_AAB_AAAIgrBWN_4eDwWC2A==\",\"boredomJson\":\"0\",\"nameJSON\":\"Karkat Vantas\",\"healthJson\":\"100\",\"TYPE\":\"GRUB\",\"corrupt\":\"false\",\"purified\":\"false\",\"patience\":\"0\",\"idealistic\":\"0\",\"curious\":\"0\",\"loyal\":\"113\",\"energetic\":\"0\",\"external\":\"0\",\"remembered\":\"{}\",\"rememberedNames\":\"{}\",\"rememberedCastes\":\"{}\"}","breeder":"ParadoxSpace"}')
+permanents = [karkat,feferi,terezi]
+
 #TimeHole.create(permanent: true , wigglerJSON: '')
-TimeHole.create(permanent: true, wigglerJSON:'{\"lastPlayed\":\"1542843765974\",\"isempress\":\"false\",\"hatchDate\":\"1542843765974\",\"lastFed\":\"1542843765974\",\"dollDATAURL\":\" Feferi Peixes:___HBSZAE2xEmL_qeUA-lEEsj7_qeX_uin_uimZAE0AAAAA3v43qskREREuLi6xEmKZAE3ExMSZAE13ACsIgsBYOgqEBQKcJhQKAkBJgA==\",\"boredomJson\":\"0\",\"nameJSON\":\" Feferi Peixes\",\"healthJson\":\"100\",\"TYPE\":\"GRUB\",\"corrupt\":\"false\",\"purified\":\"false\",\"patience\":\"0\",\"idealistic\":\"0\",\"curious\":\"0\",\"loyal\":\"0\",\"energetic\":\"113\",\"external\":\"0\",\"remembered\":\"{}\",\"rememberedNames\":\"{}\",\"rememberedCastes\":\"{}\"}","breeder":"ParadoxSpace"}')
+
+#keeps it from remaking seeds already in the system
+permanents.each do |x|
+  if(!TimeHole.exists?(wigglerJson: x))
+    TimeHole.create(permanent: true , wigglerJSON: x)
+  end
+end
 
 
