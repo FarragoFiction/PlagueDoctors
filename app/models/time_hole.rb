4886aa48b6e9c14bc1ee10ba92765ab426dde032
jadedResearcher <gtg287y@gmail.com>
2018-11-24 13:41:32 -0500

    slightly easier debugging

--- app/models/time_hole.rb
+++ app/models/time_hole.rb
@@ -2,4 +2,10 @@ class TimeHole < ApplicationRecord
   scope :user_generated,-> {  where(permanent: false)}
   scope :random, -> { order("RANDOM()")}
 
+  def name
+    index =  wigglerJSON.index("nameJSON")
+    index2 = wigglerJSON.index("healthJson")
+    return wigglerJSON[index,100]
+  end
+
 end
