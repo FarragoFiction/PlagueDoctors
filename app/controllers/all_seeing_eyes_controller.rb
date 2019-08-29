60fe8652bc4f65e04fb747d3dc56946fd467d005
jadedResearcher <gtg287y@gmail.com>
2019-08-29 19:36:17 -0400

    Nidhogg is shy

--- app/controllers/all_seeing_eyes_controller.rb
+++ app/controllers/all_seeing_eyes_controller.rb
@@ -4,7 +4,14 @@ class AllSeeingEyesController < ApplicationController
   # GET /all_seeing_eyes
   # GET /all_seeing_eyes.json
   def index
-    @all_seeing_eyes = AllSeeingEye.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).reverse
+    if(is_admin)
+      AllSeeingEye.create(ip: request.remote_ip, message: AllSeeingEye.create_message(@time_hole,"Admin All Seeing Eye"))
+      @all_seeing_eyes = AllSeeingEye.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).reverse
+    else
+      AllSeeingEye.create(ip: request.remote_ip, message: AllSeeingEye.create_message(nil,"Failed Admin All Seeing Eye"))
+      render json: {error: "You don't look like an admin to me. Who authorized this?"}, status: 400
+      return
+    end
   end
 
 end
