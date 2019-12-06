25e27fa3952e22f6a2d35a832f3ce52f0b4a8ddf
jadedResearcher <gtg287y@gmail.com>
2019-12-05 19:38:18 -0500

    seed test

--- app/models/time_hole.rb
+++ app/models/time_hole.rb
@@ -11,7 +11,7 @@ class TimeHole < ApplicationRecord
   #
   after_create do
     #you can't undo your sins
-    if(corrupt && caretaker && !caretaker.corrupt)
+    if(corrupt && caretaker && !caretaker.corruption_source)
       puts "JR NOTE: OWO WHATS THIS, NOTICING YOUR SINS #{corrupt} #{caretaker_id}"
       caretaker.corruption_source = true
       caretaker.save
